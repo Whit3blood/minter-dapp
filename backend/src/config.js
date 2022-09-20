5cf72fcaeafd69a5522a258f67e3d2ef80308e56
Whit3blood <rithysatyanhek@gmail.com>
2022-09-20 23:05:55 +0700

    update

--- backend/src/config.js
+++ backend/src/config.js
@@ -57,10 +57,10 @@ const MINT_PRICE = 0; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, P
 const TOKENS_PER_MINT = 1000; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-09-20T11:30:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-09-24T11:30:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-09-22T11:30:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 7500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x7Fb71f35Aec9Db550842f9A3c15EBe33Dc18bCB4"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0x82a6bC561361c059012256A321161e17d4a56e55"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Egalitarian World NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'Egalitarian World is a simple-yet-unique collection created by Whit3blood, a dark skin Asian who believes in the equality of humankind no matter what their skin color, religion and where they were born.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
