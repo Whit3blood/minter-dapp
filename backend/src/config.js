90b43d3e0e33ae7dffe605236c6726f0283e519f
Whit3blood <rithysatyanhek@gmail.com>
2022-09-23 15:35:12 +0700

    update royalty

--- backend/src/config.js
+++ backend/src/config.js
@@ -61,7 +61,7 @@ const PUBLIC_MINT_START_DATE = "2022-09-24T11:30:48+00:00"; // This is required.
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2022-09-23T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
-const ROYALTY_SHARE = 7500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const ROYALTY_SHARE = 750; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x7Fb71f35Aec9Db550842f9A3c15EBe33Dc18bCB4"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
