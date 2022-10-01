50f0fa379446c4d956be54a40e3627abd16b4ad1
Whit3blood <rithysatyanhek@gmail.com>
2022-10-01 16:37:11 +0700

    update

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -155,7 +155,9 @@ async function checkChain() {
 }
 
 async function loadInfo() {
+  console.log('before await')
   window.info = await window.contract.methods.getInfo().call();
+  console.log('after await')
   const publicMintActive = await contract.methods.mintingActive().call();
   const presaleMintActive = await contract.methods.presaleActive().call();
   const mainHeading = document.getElementById("mainHeading");
